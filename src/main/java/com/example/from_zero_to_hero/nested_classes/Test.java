b494f113e203122d0b5d287e84e750b47eef7c05
Vitaliy Oleinik <oleynik624@gmail.com>
2022-08-29 11:20:07 +0600

    static nested class: Example of using it

---
+++ src/main/java/com/example/from_zero_to_hero/nested_classes/Test.java
@@ -0,0 +1,9 @@
+package com.example.from_zero_to_hero.nested_classes;
+
+public class Test {
+    static class A {}
+    class B {}
+    void method() {
+        class C {} // local class C
+    }
+}
---
+++ src/main/java/com/example/from_zero_to_hero/nested_classes/static_nested_class/Car.java
@@ -0,0 +1,45 @@
+package com.example.from_zero_to_hero.nested_classes.static_nested_class ;
+
+public class Car {
+    String color;
+    String doorCount;
+    Engine engine;
+
+    public Car(String color, String doorCount, Engine engine) {
+        this.color = color;
+        this.doorCount = doorCount;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "My car{" +
+                "color='" + color + '\'' +
+                ", doorCount='" + doorCount + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public static class Engine {
+        int horsePower;
+
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "My engine{" +
+                    "horsePower=" + horsePower +
+                    '}';
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine(1000);
+        Car car = new Car("red", "4", engine);
+        System.out.println(car);
+    }
+}
