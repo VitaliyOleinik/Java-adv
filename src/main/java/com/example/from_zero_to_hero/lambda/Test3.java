d4c358f624f55f677c0b4a7e2b970fbbfde39758
Vitaliy Oleinik <oleynik624@gmail.com>
2022-09-11 01:22:20 +0600

    lambda: Supplier functional interface with get method

---
+++ src/main/java/com/example/from_zero_to_hero/lambda/Test3.java
@@ -0,0 +1,40 @@
+package com.example.from_zero_to_hero.lambda;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+public class Test3 {
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
+        ArrayList<Car> a1 = new ArrayList<>();
+        for (int i = 0; i < 3; i ++) {
+            a1.add(carSupplier.get());
+        }
+        return a1;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Car> cars = createThreeCars(() -> new Car("Toyota", "white", 4.2));
+        System.out.println("Our cars: " + cars);
+    }
+}
+
+class Car {
+    String model;
+    String color;
+    double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+}
