af4f5cfc4d3301cf34991f2a12781fc518e09039
Vitaliy Oleinik <oleynik624@gmail.com>
2022-09-16 10:37:00 +0600

    stream: method filter

---
+++ src/main/java/com/example/from_zero_to_hero/stream/Test2.java
@@ -0,0 +1,94 @@
+package com.example.from_zero_to_hero.stream;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Student student1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student student2 = new Student("Kit", 'f', 52, 4, 5.3);
+        Student student3 = new Student("Hello", 'm', 12, 1, 3.3);
+        Student student4 = new Student("Ketto", 'f', 44, 2, 9.3);
+
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+
+        students = students.stream().filter(element ->
+                element.getAge() > 22 && element.getAvgGrade() < 8)
+                .collect(Collectors.toList());
+        System.out.println(students);
+    }
+
+}
+
+class Student {
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}' + '\n';
+    }
+}
