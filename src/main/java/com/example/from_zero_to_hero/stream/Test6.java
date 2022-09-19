bd2ac8441d78990db8ae57228884583a8b5823fe
Vitaliy Oleinik <oleynik624@gmail.com>
2022-09-19 21:43:42 +0600

    stream: concat method

--- src/main/java/com/example/from_zero_to_hero/stream/Test6.java
+++ src/main/java/com/example/from_zero_to_hero/stream/Test6.java
@@ -12,5 +12,10 @@ public class Test6 {
             return element%2==0;
         }).collect(Collectors.toList());
         System.out.println(list);
+
+        Stream<Integer> stream2 = Stream.of(1,2,3,4,5,6,7,8,9);
+        Stream<Integer> stream3 = Stream.of(9,8,7,6,5,4,3,2,1);
+        Stream<Integer> stream4 = Stream.concat(stream2, stream3);
+        stream4.forEach(System.out::println);
     }
 }
