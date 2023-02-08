f52b59c3f991a8084d593a9e11a8d35eb9a0870b
Vitaliy Oleinik <oleynik624@gmail.com>
2023-02-08 23:12:04 +0600

    concurrency introduction

---
+++ src/main/java/com/example/from_zero_to_hero/multithreading/Ex1.java
@@ -0,0 +1,11 @@
+package com.example.from_zero_to_hero.multithreading;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Privet");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n Poka");
+    }
+}
