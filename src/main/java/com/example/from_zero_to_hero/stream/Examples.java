34b1341a3aebebea0431ae1209733524df05894b
Vitaliy Oleinik <oleynik624@gmail.com>
2023-02-25 23:51:58 +0600

    stream of array Stream<String[]> to Stream<String>

--- src/main/java/com/example/from_zero_to_hero/stream/Examples.java
+++ src/main/java/com/example/from_zero_to_hero/stream/Examples.java
@@ -1,8 +1,14 @@
 package com.example.from_zero_to_hero.stream;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Examples {
     List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
@@ -146,4 +152,29 @@ class ConvertListToMap {
         System.out.println("map: " + mapInitCards2Length);
     }
 
+}
+
+class FlatMapExample3 {
+
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("C:\\test\\test.txt");
+
+        // read file into a stream of lines
+        Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8);
+
+        // stream of array...hard to process.
+        // Stream<String[]> words = lines.map(line -> line.split(" +"));
+
+        // stream of stream of string....hmm...better flat to one level.
+        // Stream<Stream<String>> words = lines.map(line -> Stream.of(line.split(" +")));
+
+        // result a stream of words, good!
+        Stream<String> words = lines.flatMap(line -> Stream.of(line.split(" +")));
+
+        // count the number of words.
+        long noOfWords = words.count();
+
+        System.out.println(noOfWords);  // 16
+    }
 }
