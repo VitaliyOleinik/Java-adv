50d769b88e86ab3c18f7ffbc06bcc4dc264ae96d
Vitaliy Oleinik <oleynik624@gmail.com>
2023-05-20 21:31:40 +0600

    new examples for regex

--- src/main/java/com/example/from_zero_to_hero/regex/Regex2.java
+++ src/main/java/com/example/from_zero_to_hero/regex/Regex2.java
@@ -17,8 +17,44 @@ public class Regex2 {
 //        String s1 = "abcd abce abc5abcg6abch";
 //        Pattern pattern1 = Pattern.compile("abc[^e-g4-7]"); // ^ отрицание
 
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc(e|5)");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc."); // . любой символ
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("^abc"); // начало строки
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc5abcg6abch$"); // конец строки
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\d"); // одна цифра
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\D"); // все без цифр
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\w"); // [A-Za-z0-9_]
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\w+"); // Position: 0   abcd
+                                                    // Position: 5   abce
+                                                    // Position: 10   abc5abcg6abch
+//        String s1 = "abcd@#!#@ #$@ ab=====ce a124214@#!@$#(*&^%$^bc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\W+"); // символ
+
+//        String s1 = "poka abc Vitaliiy dom kino";
+//       Pattern pattern1 = Pattern.compile("\\w{4}");
+                                                        /*
+                                                        Position: 0   poka
+                                                        Position: 9   Vita
+                                                        Position: 13   liiy
+                                                        Position: 22   kino*/
+
         String s1 = "abcd abce abc5abcg6abch";
-        Pattern pattern1 = Pattern.compile("abc(e|5)");
+        Pattern pattern1 = Pattern.compile("\\d"); // одна цифра
 
         Matcher matcher = pattern1.matcher(s1);
 
