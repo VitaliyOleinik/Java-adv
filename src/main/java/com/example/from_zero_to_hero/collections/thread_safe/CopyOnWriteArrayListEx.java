88d24e9f204b6f61cba504ba470a2cb1a6705d49
vit <oleynik624@gmail.com>
2024-11-01 21:53:22 +0500

    CopyOnWriteArrayList

---
+++ src/main/java/com/example/from_zero_to_hero/collections/thread_safe/CopyOnWriteArrayListEx.java
@@ -0,0 +1,46 @@
+package com.example.from_zero_to_hero.collections.thread_safe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListEx {
+    public static void main(String[] args) throws InterruptedException {
+//        ArrayList<String> list = new ArrayList<>();
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("Vit");
+        list.add("Petya");
+        list.add("Ivan");
+        list.add("Antony");
+        list.add("John");
+        System.out.println(list)
+        ;
+        Runnable runnable1 = () -> {
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(iterator.next());
+            }
+        };
+        Runnable runnable2 = () -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list.remove(2);
+            list.add("Elena");
+        };
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(list);
+    }
+}
